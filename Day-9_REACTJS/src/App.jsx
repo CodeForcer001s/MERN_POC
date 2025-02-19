5291f5da4fa9c6aec6f47e86d686658971d58e08
S Jegannath <sjegannath56@gmail.com>
2025-02-19 23:29:02 +0530

    Day 9 Work AN

---
+++ Day-9_REACTJS/src/App.jsx
@@ -0,0 +1,40 @@
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Login from './components/Auth/Login'
+import Signup from './components/Auth/Signup'
+import { BrowserRouter as Router, Route,Routes, Link } from "react-router-dom";
+const App = () => {
+  return (
+    <Router>
+      <div className='jegan'>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contacts">Contacts</Link>
+            </li>
+            <li>
+              <Link to="/help-desk">Help Desk</Link>
+            </li>
+          </ul>
+        </nav>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contacts" element={<Contact />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      
+    </Router>
+  );
+};
+
+export default App;
---
+++ Day-9_REACTJS/src/components/Auth/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+      First time??? [Signup]
+    </div>
+  )
+}
+
+export default Signup
