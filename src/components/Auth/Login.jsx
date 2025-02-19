ca26343ad64a6fb77d1a8baa27ca2990f00dd9ba
S Jegannath <sjegannath56@gmail.com>
2025-02-19 23:17:27 +0530

    Day 9 FN

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      Work with me [Login]
+    </div>
+  )
+}
+
+export default Login
---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      Without Me [contact]
+    </div>
+  )
+}
+
+export default Contact
